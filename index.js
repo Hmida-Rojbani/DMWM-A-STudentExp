ba00c314ca18c12ebe536e3268602387a7ea15f1
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-04-07 14:05:09 +0100

    preparing express app

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 require('./db/connection')('mongodb+srv://user:1234@mongoformation-xc16w.mongodb.net/dmwmA?retryWrites=true&w=majority')
+const express = require('express');
+const app_debug = require('debug')('app:debug');
+const app = express();
+const morgan = require('morgan');
+const port = process.env.PORT | 3000;
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+
+app.listen(port, ()=> app_debug(`Serveur running on ${port}...`));
