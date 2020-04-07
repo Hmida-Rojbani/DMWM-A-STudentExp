cf2327bd3325da2de9f084e13f083c7783f9f852
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-04-07 14:46:42 +0100

    utilisation de lodash

--- routers/students.js
+++ routers/students.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Student } = require('../models/student');
+const _ = require('lodash');
 
 router.get('',async (req,res)=>{
     const students = await Student.find();
@@ -10,12 +11,14 @@ router.get('',async (req,res)=>{
 
 router.post('',async (req,res)=>{
     //request body content the data 
-    const student = new Student({
+    /*const student = new Student({
         name: req.body.name,
         age: req.body.age,
         email: req.body.email
-    });
+    });*/
 
+    //validation par joi
+    const student = new Student(_.pick(req.body,['name','age','email']));
     const saved_student = await student.save();
     res.status(201).send(saved_student);
 });
