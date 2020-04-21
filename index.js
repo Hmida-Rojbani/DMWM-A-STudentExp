990d258cc0da67eff97e9cfb742934e32dedb286
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-04-21 15:21:58 +0100

    creation of users to complet it

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const morgan = require('morgan');
 const port = process.env.PORT | 3000;
 const student_router = require('./routers/students');
 const class_room_router = require('./routers/class_rooms');
+const user_router = require('./routers/users');
 
 app.use(express.json());
 app.use(morgan('dev'));
 
 app.use('/api/students',student_router);
 app.use('/api/class_rooms',class_room_router);
+app.use('/api/users',user_router);
 
 
 app.listen(port, ()=> app_debug(`Serveur running on ${port}...`));
---
+++ routers/users.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { User, user_not_valide } = require('../models/user');
+const _ = require('lodash');
+
+//register
+
+router.post('/register',(req,res)=>{
+    
+})
+
+
+
+
+
+
+module.exports = router;
