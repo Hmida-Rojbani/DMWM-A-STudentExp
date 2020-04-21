2f1612a72a9c24b1440d3dcd757c8d0074b46aaa
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-04-21 16:00:33 +0100

    Update users.js

--- routers/users.js
+++ routers/users.js
@@ -5,12 +5,28 @@ const bcrypt = require('bcrypt');
 
 //register
 
-router.post('/register',(req,res)=>{
+router.post('/register',async (req,res)=>{
+    let errors;
+    if(errors=user_not_valide(req.body))
+        return res.status(400).send(errors.details[0].message);
+    let user = new User(_.pick(req.body,['name','email','password','isAdmin']));
+
+    let salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    try{
+        user = await user.save();
+        return res.status(201).send('User registred');
+    }catch(err){
+        return res.status(400).send('DB Error :'+ err.message);
+    }
 
 })
 
 
+//login
 
+router.post('/login',async (req,res)=>{
+});
 
 
 
