2746123e532c96baf4bb8be96e845270fe8a91bc
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-04-21 15:22:53 +0100

    Update users.js

--- routers/users.js
+++ routers/users.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const { User, user_not_valide } = require('../models/user');
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
 
 //register
 
 router.post('/register',(req,res)=>{
-    
+
 })
 
 
