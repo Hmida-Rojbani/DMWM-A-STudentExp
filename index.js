b5373a739f7939c8393a7d9be9f509f8251ffffb
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-05-12 13:50:22 +0100

    caught other type of exception

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require('express-async-errors');
 const error = require('./middlewares/error');
 const config = require('config');
+if(!process.env.secret_jwt || !process.env.secret_mongo){
+    console.log('Variables are not set');
+    process.exit(0);
+}
 require('./db/connection')(config.get('db.prot')+config.get('db.coord')+config.get('db.path'))
 const express = require('express');
 const app_debug = require('debug')('app:debug');
@@ -15,10 +19,26 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 
+
+process.on('uncaughtException', (err) =>{
+    console.log('Something failed in the Server.'+err.message);
+});
+
+process.on('unhandledRejection', (ex) =>{
+    console.log('Something rejected in the Server.'+ex.message);
+});
+
+
+// throw new Error('Something');
+
 app.use('/api/students',student_router);
 app.use('/api/class_rooms',auth,class_room_router);
 app.use('/api/users',user_router);
 app.use(error);
+
+
+
+
 
 
 app.listen(port, ()=> app_debug(`Serveur running on ${port}...`));
